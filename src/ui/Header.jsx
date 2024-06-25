02dc96efa18d014c90a8437610611bd83af91975
Mohamed Atif <mohamedabotreka40@gmail.com>
2024-06-25 23:53:03 +0300

    new-commit

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -5,7 +5,7 @@ import Username from "../features/user/Username";
 function Header() {
   return (
     <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
-      <Link className="tracking-widest">Fast React Pizza Co.</Link>
+      <Link className="tracking-widest">Fast Pizza Co.</Link>
       <SearchOrder />
       <Username />
     </header>
